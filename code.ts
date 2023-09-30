0db8d8f08a81935faa574ae5735bdc9a7b42714e
Michel Ledig <m.ledig.mic@gmail.com>
2023-09-30 17:10:04 -0300

    feature: creating instances of node components on the project

--- code.ts
+++ code.ts
@@ -1,3 +1,21 @@
 console.log("[FIGMA PLUGIN] Showing the UI...")
 figma.showUI(__html__)
 figma.ui.resize(500, 500)
+
+figma.ui.onmessage = pluginMessage => {
+
+    const postComponentSet = figma.root.findOne(node=> node.type === "COMPONENT_SET" && node.name === "post") as ComponentSetNode;
+
+    const defaultVariant = postComponentSet.defaultVariant as ComponentNode;
+    const darkModeVariant = postComponentSet.findOne(node => node.type === "COMPONENT" && node.name === "Image=none, Dark mode=true") as ComponentNode
+
+
+    if(pluginMessage.darkModeState){
+        darkModeVariant.createInstance();
+    } else{
+        defaultVariant.createInstance();
+    }
+    console.log(pluginMessage);
+
+    figma.closePlugin();
+}
